40cd844b004c3d78b9528ea3efbc2867e230bc00
Bryan Crowe <bryan@bryan-crowe.com>
2023-10-07 14:13:53 -0400

    Add Bryan to sample user data

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -2,6 +2,7 @@ import { User } from '../interfaces'
 
 /** Dummy user data. */
 export const sampleUserData: User[] = [
+  { id: 69, name: 'Bryan' },
   { id: 101, name: 'Alice' },
   { id: 102, name: 'Bob' },
   { id: 103, name: 'Caroline' },
